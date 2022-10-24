de0ddcb74638b31a8273005350f841cad407b2fc
carohavenstein <carohavenstein@gmail.com>
2022-10-24 10:19:19 -0300

    funcion parseArgumentos y ejecutarArgumentos empezadas

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -3,71 +3,69 @@
 #include <string.h>
 #include <ctime>
 #include <sstream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-class Opciones {};
+/*
+funcion:
+construye el arbol
+cuenta cantidad de letras palabras y lineas del .txt
+devuelve el arbol de palabras diferentes
+*/
+//Arbol<string>
+void construirArbol(ifstream& archivo, int& contLineas, int& contPalabras, int& contLetras) {
+            
+    string linea;
 
-class MainLoop {
-    private:
-    int letras = 0;
-    int palabras = 0;
-    int lineas = 0;
-    // Arbol arbol;
-    Opciones opciones;
+    while(!archivo.eof()) { //mietras no sea el final del archivo
+        
+        getline(archivo,linea);
+        contLineas++;
+        //cout<<linea<<endl;
+        //Contador Letras
 
-    public:
-        MainLoop() {}
-        MainLoop(Opciones ops)
-            : opciones(ops) {}
+        string palabra = "";
 
-        void construirArbol(ifstream& archivo) {
-            string linea;
-
-            while(!archivo.eof()) { //mietras no sea el final del archivo
-                getline(archivo,linea);
-                lineas++;
-                //cout<<linea<<endl;
-                //Contador Letras
-                string palabra2 = "";
-                for (int i = 0; i < linea.length()+1; i++){
-                    // cout << linea[i]  << " ";
-                    if (isalpha(linea[i])){
-                        letras += 1;
-                        palabra2 += linea[i];
-                    } else {
-                        if (palabra2 != "") {
-                            //arbol.agregar(palabra2); !!!!!!!!!!!!!!
-                            // cout << palabra2 << endl;
-                            palabras++;
-                        }
-                        palabra2 = "";
-                    }
+        for (int i = 0; i < linea.length()+1; i++){
+            // cout << linea[i]  << " ";
+            if (isalpha(linea[i])){
+                contLetras += 1;
+                palabra += linea[i];
+            } else {
+                if (palabra != "") {
+                    //arbolPalabrasDif.put(palabra); !!!!!!!!!!!!!!
+                    // cout << palabra << endl;
+                    contPalabras++;
                 }
-
+                palabra = "";
             }
         }
 
-        void EjecutarOpciones(Opciones opciones);
+    }
 
-        int Letras() { return letras; }
-        int Palabras() { return palabras; }
-        int Lineas() { return lineas; }
+    //return arbolPalabrasDif;
 
+}
+
+enum class ArgType {
+    Nulo = 0,
+    Palabras,
+    Ocurrencias,
+    Mostrar,
+    Excluir,
+    ExcluirF
 };
 
-/*
-static const string argumentos[] = {
-    "palabras",
-    "explotar"
+struct Argumento {
+    ArgType id = ArgType::Nulo;
+    bool activo;
+    int n;
+    string palabras;
 };
 
-*/
-
-
-
-
-void parseArgumentos(int argc, char* argv[]) {
-
+unordered_map<string, Argumento> parseArgumentos(int argc, char* argv[]) {
+    unordered_map<string, Argumento> argumentos;
     for (int i = 0; i < argc; i++) {
 
         string arg(argv[i]); //convierto puntero a caracter en string para comparar los contenidos
@@ -90,6 +88,11 @@ void parseArgumentos(int argc, char* argv[]) {
                 i++;
                 cantMostrar = stoi(argumentoSig); //string to int
             }
+
+            Argumento arg;
+            arg.id = ArgType::Palabras;
+            arg.n = cantMostrar;
+            argumentos["palabras"] = arg;
            
             if(cantMostrar == 0) {
                 //imprimir todo el arbol en orden alfabetico
@@ -123,47 +126,29 @@ void parseArgumentos(int argc, char* argv[]) {
             }
         }
     }
+
+    return argumentos;
 }
 
-/*
-class Handler {
-    string arg;
-    Func func;
-};
-
-static const Handler handlers[] = {
-    {"palabras", HandlePalabras},
-}
-
-void HandlePalabras(int argp, char* argv[]) {
-
-}
-
-void parseArgumentos(int argc, char* argv[]) {
-    for (int i = 0; i < argc; i++) {
-        string arg(argv[i]); //convierto puntero a caracter en string para comparar los contenidos
-        for (auto& handler : handlers) {
-            if (handler.arg == arg) {
-                handler.func(i, argv);
-            }
-        }
+void EjecutarArgumentos(unordered_map<string, Argumento> args) {
+    Argumento palabras = args["palabras"];
+    if (palabras.id == ArgType::Palabras) {
+        // mostrarPalabras(palabras.n);
     }
 }
-
-*/
-
 
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        cout << "error" << endl;
-        return -1;
-    }
 
     clock_t begin;
-
     cout << "Comenzando a medir Tiempo\n" << endl;
-
     begin = clock();
+
+
+    int contLineas = 0;
+    int contPalabras = 0;
+    int contLetras = 0;
+    
+    //Arbol arbolPalabrasDif;
 
     ifstream archivo;
     archivo.open(argv[1],ios::in); //abrimos el archivo en modo lectura 
@@ -173,24 +158,22 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    MainLoop loop;
-    loop.construirArbol(archivo);
-    //construir arbol ocurrencias tambien;
-    loop.EjecutarOpciones(Opciones{});
+    
+    //arbolPalabrasDif = construirArbol(archivo, contLineas, contPalabras, contLetras);
+    construirArbol(archivo, contLineas, contPalabras, contLetras);
 
-    cout<<"\nNumero de LINEAS que tiene el archivo es de: " <<loop.Lineas()<<endl;
-    cout<<"\nNumero de PALABRAS que tiene el archivo es de: " <<loop.Palabras()<<endl;
-    cout<<"\nNumero de LETRAS que tiene el archivo es de: " <<loop.Letras()<<endl;
+    cout<<"\nNumero de LINEAS que tiene el archivo es de: " <<contLineas<<endl;
+    cout<<"\nNumero de PALABRAS que tiene el archivo es de: " <<contPalabras<<endl;
+    cout<<"\nNumero de LETRAS que tiene el archivo es de: " <<contLetras<<endl;
 
     archivo.close(); //cerramos el archivo
 
-    
+    auto argumentos = parseArgumentos(argc, argv);
+    EjecutarArgumentos(argumentos);    
+
     clock_t end = clock();
-
     double elapsed_secs = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
-
     cout << "Tardo elapsed_secs" << elapsed_secs << "\n" << std::endl;
 
     return 0;
-
 }
