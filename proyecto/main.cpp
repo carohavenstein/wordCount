d89e6d3143e3e5593f2405f8325c4df2a96126ee
carohavenstein <carohavenstein@gmail.com>
2022-10-26 19:40:36 -0300

    cambio logica para hash

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -3,18 +3,15 @@
 #include <string.h>
 #include <ctime>
 #include <sstream>
-#include <vector>
 #include <unordered_map>
 using namespace std;
 
 /*
 funcion:
-construye el arbol
-cuenta cantidad de letras palabras y lineas del .txt
-devuelve el arbol de palabras diferentes
+cuenta cantidad de letras palabras y lineas del file.txt
+cuenta cantidad de palabras diferentes
 */
-//Arbol<string>
-void construirArbol(ifstream& archivo, int& contLineas, int& contPalabras, int& contLetras) {
+void funcionesBasicas(ifstream& archivo, int& contLineas, int& contPalabras, int& contLetras, int& contPalabrasDif) {
             
     string linea;
 
@@ -22,20 +19,17 @@ void construirArbol(ifstream& archivo, int& contLineas, int& contPalabras, int& 
         
         getline(archivo,linea);
         contLineas++;
-        //cout<<linea<<endl;
-        //Contador Letras
 
         string palabra = "";
 
         for (int i = 0; i < linea.length()+1; i++){
-            // cout << linea[i]  << " ";
+
             if (isalpha(linea[i])){
                 contLetras += 1;
                 palabra += linea[i];
             } else {
                 if (palabra != "") {
-                    //arbolPalabrasDif.put(palabra); !!!!!!!!!!!!!!
-                    // cout << palabra << endl;
+                    //meterla en el hash map; !!!!!!!!!!!!!!
                     contPalabras++;
                 }
                 palabra = "";
@@ -44,7 +38,74 @@ void construirArbol(ifstream& archivo, int& contLineas, int& contPalabras, int& 
 
     }
 
-    //return arbolPalabrasDif;
+    //contPalabrasDif = cantpalabrasdif del hashmap
+
+}
+
+
+//deberia devolver el hashmap
+void funcionExcluir(string palabrasArgv) {
+    
+    //trasnformo argv de palabras a expluir en stringstream: son las palabras a excluir separadas por ,
+    stringstream palabrasExcluir(palabrasArgv);
+    string aExcluir;
+
+    while (getline(palabrasExcluir, aExcluir, ',')) {
+        cout<<"palabra a excluir: "<<aExcluir<<endl;
+        //aEliminar.remove()
+        //va eliminando las palabras del hashmap
+        //para que despues comandos palabras y ocurrencias no las muestren
+    }
+    
+}
+
+//deberia devolver el hasmap
+void funcionExcluirF(string palabrasArgv){
+    ifstream archivo;
+    //abrimos el archivo en modo lectura 
+    archivo.open(palabrasArgv,ios::in);
+        
+    if (archivo.fail()) {
+        cout<<"No se pudo abrir el archivo";
+        exit(1);
+    }
+
+    string linea;
+
+    while(!archivo.eof()) { //mietras no sea el final del archivo
+        
+        getline(archivo,linea);
+
+        string palabra = "";
+
+        for (int i = 0; i < linea.length()+1; i++){
+
+            if (isalpha(linea[i])){
+                palabra += linea[i];
+            } else {
+                if (palabra != "") {
+                    cout << "palabra a excluir del file ign.txt: " << palabra << endl;
+                    //eliminarla del hashmap
+                }
+                palabra = "";
+            }
+        }
+
+    }
+}
+
+void funcionMostrar(string palabrasArgv){
+
+    //trasnformo argv de palabras a expluir en stringstream: son las palabras a excluir separadas por ,
+    stringstream palabrasMostrar(palabrasArgv);
+    string aMostrar;
+    int size = 0;
+    while (getline(palabrasMostrar, aMostrar, ',')) {
+        size ++;
+        //aMostrar.search() la busco en el hashmap
+        //que vaya guardando las palabras a mostrar en algun lado
+        //despues hay que ordenalas por ocurrencia (con quicksort?) y mostrarlas
+    }
 
 }
 
@@ -61,11 +122,11 @@ struct Argumento {
     ArgType id = ArgType::Nulo;
     bool activo;
     int n;
-    string palabras;
+    string palabrasArgv;
 };
 
 unordered_map<string, Argumento> parseArgumentos(int argc, char* argv[]) {
-    
+
     unordered_map<string, Argumento> argumentos;
 
     for (int i = 0; i < argc; i++) {
@@ -74,14 +135,12 @@ unordered_map<string, Argumento> parseArgumentos(int argc, char* argv[]) {
 
         if (arg == "-palabras") {
             
-            cout << "palabras estaba en argumentos" << endl;
-
             int cantMostrar = 0;
             //si el primer caracter del siguiente argv es un numero
             //y que exista el siguiente argv, porque puede no haber
             if ((i+1 < argc) && isdigit(argv[i+1][0])) {
                 string argumentoSig(argv[i+1]);
-                //i++; no se si esto funciona
+                i++;
                 cantMostrar = stoi(argumentoSig); //string to int
             }
 
@@ -91,8 +150,6 @@ unordered_map<string, Argumento> parseArgumentos(int argc, char* argv[]) {
             argumentos["-palabras"] = arg;
             
         } else if (arg == "-ocurrencias") {
-
-            cout << "ocurrencias estaba en argumentos" << endl;
 
             int cantMostrar = 0;
             //si el primer caracter del siguiente argv es un numero
@@ -107,58 +164,38 @@ unordered_map<string, Argumento> parseArgumentos(int argc, char* argv[]) {
             arg.id = ArgType::Ocurrencias;
             arg.n = cantMostrar;
             argumentos["-ocurrencias"] = arg;
+            i++;
 
-            /*
-                if(cantMostrar == 0) {
-                    //mostrar todas las palabras ordenadas segun ocurrencia creciente
-                } else {
-                    //mostrar n palabras segun ocurrencia creciente
-                } 
-            */
+        } else if (arg == "-mostrar") { //-mostrar "palabra, palabra" segun ocurrencia creciente
 
-        } else if (arg == "-mostrar") { // -mostrar "palabra, palabra" segun ocurrencia creciente
+            Argumento arg;
+            arg.id = ArgType::Mostrar;
+            arg.palabrasArgv = string(argv[i+1]); //transformo el siguiente argv en string (palabras a mostrar)
+            argumentos["-mostrar"] = arg;
+            i++;
 
-            cout << "mostrar estaba en argumentos" << endl;
-
-            stringstream palabrasMostrar(argv[i+1]); //trasnformo arg siguiente en stringstream: son las palabras a mostrar separadas por ,
-            string aMostrar;
-            while (getline(palabrasMostrar, aMostrar, ',')) {
-                //aMostrar.search() la busco en el arbol ordenado alfbtcmt, para obtener la palabra con las ocurrencias?
-                //que vaya guardando las palabras a mostrar en algun lado
-                //despues hay que ordenalas por ocurrencia (con quicksort?) y mostrarlas
-            }
         } else if (arg == "-excluir") { //-excluir "palabra, palabra"
-            //eliminar esas palabras del arbol
-
-            cout << "excluir estaba en argumentos" << endl;
 
             Argumento arg;
             arg.id = ArgType::Excluir;
-            arg.palabras = string(argv[i+1]); //transformo el siguiente argv en string (palabras a excluir)
+            arg.palabrasArgv = string(argv[i+1]); //transformo el siguiente argv en string (palabras a excluir)
             argumentos["-excluir"] = arg;
-
             i++;
-
-            /*
-                stringstream palabrasExcluir(argv[i+1]); //trasnformo arg siguiente en stringstream: son las palabras a excluir separadas por ,
-                string aExcluir;
-                while (getline(palabrasExcluir, aExcluir, ',')) {
-                //aEliminar.remove()
-                // va eliminando las palabras del arbol ordenado alfbtcmt
-                //para que despues comandos palabras y ocurrencias no las muestren
-            */
            
-        } else if (arg == "-excluirF") {
-            //el siguiente arg es un .txt
-            cout << "excluirF estaba en argumentos" << endl;
-
+        } else if (arg == "-excluirF") { //el siguiente arg es un ign.txt
+            
+            Argumento arg;
+            arg.id = ArgType::ExcluirF;
+            arg.palabrasArgv = string(argv[i+1]);
+            argumentos["-excluir"] = arg;
+            i++;
         }
     }
 
     return argumentos;
 }
 
-void EjecutarArgumentos(unordered_map<string, Argumento> args) { //tendria que pasarle el arbol tmb
+void ejecutarArgumentos(unordered_map<string, Argumento> args) { //pasarle el hashmap de palabras
 
     Argumento palabras = args["-palabras"];
     Argumento ocurrencias = args["-ocurrrencias"];
@@ -166,22 +203,45 @@ void EjecutarArgumentos(unordered_map<string, Argumento> args) { //tendria que p
     Argumento excluir = args["-excluir"];
     Argumento excluirF = args["-excluirF"];
 
+    if (excluir.id == ArgType::Excluir) {
+        //reciba el hashmap, y el string de palabras a ignorar y las elimine del hashmap
+        //que devuelva el hashmap, mapaPalabras = mapa devuelto.
+        funcionExcluir(excluir.palabrasArgv);
+
+    }
+
+    if (excluirF.id == ArgType::ExcluirF) {
+        funcionExcluirF(excluirF.palabrasArgv);
+        
+    }
+
     if (palabras.id == ArgType::Palabras) {
         //para controlar si existe arg palabras, si no existe argType es nulo
-        
-        // mostrarPalabras(palabras.n);
         if(palabras.n == 0) {
-            //imprimir todo el arbol en orden alfabetico
+            //ordenar todo el hashmap alfabeticamente y mostrarlo
         } else {
-            //imprimir el arbol en orden alfabetico de las n primeras palabras
+            //ordenar todo el hashmap alfabeticamente y mostrar las primeras n
         }
 
     }
 
     if (ocurrencias.id == ArgType::Ocurrencias) {
+        
+    }
 
+    if (mostrar.id == ArgType::Mostrar) {
+        
+        if(mostrar.n == 0) {
+            //mostrar todas las palabras ordenadas segun ocurrencia creciente
+        } else {
+            //mostrar n palabras segun ocurrencia creciente
+        } 
+
+        funcionMostrar(mostrar.palabrasArgv);
     }
 }
+
+
 
 int main(int argc, char** argv) {
 
@@ -193,32 +253,36 @@ int main(int argc, char** argv) {
     int contLineas = 0;
     int contPalabras = 0;
     int contLetras = 0;
-    
-    //Arbol arbolPalabrasDif;
+    int contPalabrasDif = 0;
 
-    ifstream archivo;
-    archivo.open(argv[1],ios::in); //abrimos el archivo en modo lectura 
+    if (argc == 2 && string(argv[1]) == "file.txt") {
+        
+        ifstream archivo;
+        //abrimos el archivo en modo lectura 
+        archivo.open(argv[1],ios::in);
+        
+        if (archivo.fail()) {
+            cout<<"No se pudo abrir el archivo";
+            exit(1);
+        }
 
-    if(archivo.fail()){
-        cout<<"No se pudo abrir el archivo";
-        exit(1);
+        //Arbol arbolPalabrasDif;
+        //arbolPalabrasDif = construirArbol(archivo, contLineas, contPalabras, contLetras);
+        funcionesBasicas(archivo, contLineas, contPalabras, contLetras, contPalabrasDif);
+
+        cout<<"\nNumero de LINEAS: " <<contLineas<<endl;
+        cout<<"\nNumero de PALABRAS: " <<contPalabras<<endl;
+        cout<<"\nNumero de LETRAS: " <<contLetras<<endl;
+        //cout<<"\nPalabras diferentes: " <<contPalabrasDif<<endl;
+        
+        archivo.close(); //cerramos el archivo
+
+    } else {
+        //creo el hashmap
+        auto argumentos = parseArgumentos(argc, argv);
+        ejecutarArgumentos(argumentos);
     }
-
     
-    //arbolPalabrasDif = construirArbol(archivo, contLineas, contPalabras, contLetras);
-
-    construirArbol(archivo, contLineas, contPalabras, contLetras);
-
-    cout<<"\nNumero de LINEAS: " <<contLineas<<endl;
-    cout<<"\nNumero de PALABRAS: " <<contPalabras<<endl;
-    cout<<"\nNumero de LETRAS: " <<contLetras<<endl;
-    //para mostrar cant de palabras diferentes
-    //cout<<"\nPalabras diferentes: arbolPalabrasDif->get(cantNodos);
-
-    archivo.close(); //cerramos el archivo
-
-    auto argumentos = parseArgumentos(argc, argv);
-    //EjecutarArgumentos(argumentos);    
 
     clock_t end = clock();
     double elapsed_secs = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
